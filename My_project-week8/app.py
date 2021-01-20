74fd2f4b6546974e7ab14ea7d84dcbbdc478f391
johnkim41322 <idleader@naver.com>
2021-01-20 19:45:45 +0900

    ㅇㅇ
    ㅇㅇ

---
+++ My_project-week8/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+app = Flask(__name__)
+
+@app.route('/')
+def home():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
+    return render_template('index.html')
+
+@app.route('/list')
+def list_page():  # 함수명 수정 - 이름만 보고 접속되는 페이지를 확인할 수 있게!
+    return render_template('list.html')
+
+@app.route('/areas', methods=['GET'])
+def getArea():
+    areas = list(db.areas.find({}, {"_id":False}))
+    return jsonify({'result': "success", "areas":areas})
+
+@app.route('/sizes', methods=['GET'])
+def getSize():
+    sizes = list(db.sizes.find({},{"_id":False}))
+    return jsonify({'result': "success", "sizes":sizes})
+
+@app.route('/manufacturers', methods=['GET'])
+def getManufacturers():
+    manufacturers = list(db.manufacturers.find({},{"_id":False}))
+    return jsonify({'result': "success", "manufacturers":manufacturers})
+
+@app.route('/models', methods=['GET'])
+def getModels():
+    manufacturer_idx = request.args.get("manufacturer_idx")
+    models = list(db.models.find({"manufacturer_idx":int(manufacturer_idx)},{"_id":False}))
+    return jsonify({'result': "success", "models":models})
+
+@app.route('/stores', methods=['GET'])
+def getStroes():
+    area = request.args.get("area")
+    size_idx = request.args.get("size_idx")
+    manufacturer_idx = request.args.get("manufacturer_idx")
+    model_idx = request.args.get("model_idx")
+    price = db.prices.find_one({"size_idx":int(size_idx), "manufacturer_idx":int(manufacturer_idx), "model_idx":int(model_idx)},{"_id":False})
+    stores = list(db.stores.find({"Area1":area, "WorkshopIdx":{"$in":price["WorkshopIdxs"]}},{"_id":False}))
+
+    return jsonify({'result': "success", "stores":stores})
+
+@app.route('/store', methods=['POST'])
+def setStore():
+    content = request.json
+    print(content['stores'])
+    db.stores.insert_many(content['stores'])
+    return jsonify({'result': "success"})
+
+@app.route('/tire', methods=['POST'])
+def setTire():
+    return jsonify({'result': "success"})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5002, debug=True)
---
+++ My_project-week8/data.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+areas = []
+areas.append({"area_idx":1, "text":"강원"})
+db.areas.insert_many(list(areas))
+
+sizes = []
+sizes.append({"size_idx":1, "text":"245/45/20"})
+sizes.append({"size_idx":2, "text":"255/45/20"})
+sizes.append({"size_idx":3, "text":"265/45/20"})
+db.sizes.insert_many(list(sizes))
+
+manufacturers = []
+manufacturers.append({"manufacturer_idx":1, "text":"벤츠"})
+manufacturers.append({"manufacturer_idx":2, "text":"현대"})
+manufacturers.append({"manufacturer_idx":3, "text":"기아"})
+manufacturers.append({"manufacturer_idx":4, "text":"쉐보레"})
+db.manufacturers.insert_many(list(manufacturers))
+
+models = []
+models.append({"manufacturer_idx":1, "model_idx":1, "text":"S-Class"})
+models.append({"manufacturer_idx":1, "model_idx":2, "text":"E-Class"})
+models.append({"manufacturer_idx":1, "model_idx":3, "text":"C-Class"})
+models.append({"manufacturer_idx":1, "model_idx":4, "text":"A-Class"})
+db.models.insert_many(list(models))
+
+prices = []
+prices.append({"manufacturer_idx":1, "model_idx":1, "size_idx":1, "price":"50000", "WorkshopIdxs":[1,2]})
+db.prices.insert_many(list(prices))
